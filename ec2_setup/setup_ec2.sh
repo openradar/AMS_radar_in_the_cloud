a32b546c814b763bac6a9bde91ce58fb16f8ac5d
scollis <scollis.acrf@gmail.com>
2017-01-17 07:50:56 -0600

    FIX: issues

--- ec2_setup/setup_ec2.sh
+++ ec2_setup/setup_ec2.sh
@@ -1,40 +1,55 @@
 #!/usr/bin/env bash
 #Code adapted from https://gist.github.com/yangj1e/3641843c758201ebbc6c (Modified to Python3.5)
 
+
+echo '--------'
+echo 'Apt Gets'
+echo '--------'
+
 sudo apt-get -y update
 sudo apt-get -y install gcc gfortran
 
+echo '-------------------'
+echo 'Installing Anaconda'
+echo '-------------------'
+
+
 cd ~
-#wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda2-2.4.0-Linux-x86_64.sh
-wget https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
+get https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
 bash Anaconda3-4.2.0-Linux-x86_64.sh -b
+
+#For some reason updating path and sourcing bashrc does not work
+#during the script so we use the FQ path
+
+conda_bin_path="/home/ubuntu/anaconda3/bin/" 
 
 echo "APPENDING PATH"
 cd ~
+echo 'PATH="/home/ubuntu/anaconda3/bin:$PATH"' >> .bashrc
 
 . .bashrc
 
-export VARNAME='PATH="/home/ubuntu/anaconda3/bin:$PATH"'
+#export VARNAME='PATH="/home/ubuntu/anaconda3/bin:$PATH"'
 
-/home/ubuntu/anaconda3/bin/conda update -y conda
+$conda_bin_path'conda' update -y conda
 
 #provision 
 
-/home/ubuntu/anaconda3/bin/conda install -y basemap scipy cartopy boto netCDF4
+$conda_bin_path'conda' install -y basemap scipy boto netCDF4
 
 git clone https://github.com/ARM-DOE/pyart
 
 cd ~/pyart
 
-/home/ubuntu/anaconda3/bin/python setup.py install
+$conda_bin_path'python' setup.py install
 
 cd ~
 
-/home/ubuntu/anaconda3/bin/jupyter notebook --generate-config
+$conda_bin_path'jupyter' notebook --generate-config
 
 echo "ENTER YOUR PASSWORD"
 
-key=$(/home/ubuntu/anaconda3/bin/python -c "from notebook.auth import passwd; print(passwd())")
+key=$($conda_bin_path'python' -c "from notebook.auth import passwd; print(passwd())")
 
 cd ~
 mkdir certs
